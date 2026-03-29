2376cec83d6c18b4dceffa4d3ed07f133cf4ca28
Alex <alexis.cv1824@gmail.com>
2026-03-29 11:33:54 +0300

    Lab3 Kafka cluster and producer

---
+++ producer.py
@@ -0,0 +1,23 @@
+import csv
+from kafka import KafkaProducer
+import json
+import time
+
+# Підключення до Kafka
+producer = KafkaProducer(
+    bootstrap_servers=['broker1:29092','broker2:29093'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Читаємо CSV
+with open('data.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # Відправляємо в Topic1 і Topic2
+        producer.send('Topic1', value=row)
+        producer.send('Topic2', value=row)
+        print(f"Sent: {row}")
+        time.sleep(0.1)  # щоб не навантажувати брокер
+
+producer.flush()
+producer.close()
