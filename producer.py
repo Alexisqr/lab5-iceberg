e34f3453372f162ce5002ccd30f280bc93e2c4c9
Alex <alexis.cv1824@gmail.com>
2026-06-07 13:44:50 +0300

    Lab5 Iceberg Lakehouse

--- producer.py
+++ producer.py
@@ -5,7 +5,7 @@ import time
 
 # Підключення до Kafka
 producer = KafkaProducer(
-    bootstrap_servers=['broker1:29092','broker2:29093'],
+    bootstrap_servers=['localhost:9092', 'localhost:9093'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
